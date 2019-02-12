9de9d1f2466ca1e1493c93ea0f0b717e15a1986d
David Ackerman <enfipy@gmail.com>
2019-02-12 19:35:59 +0200

    Fix

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const initBcoin = require('@/bcoin')
 const start = async () => {
   const bcoin = await initBcoin()
 
-  // if (config.bcoin.network === 'regtest') {
-  //   // To generate mock transactions for regtest node
-  //   const test = require('@/test')
-  //   await test(bcoin.nodeClient, bcoin.walletClient)
-  // }
+  if (config.bcoin.network === 'regtest') {
+    // To generate mock transactions for regtest node
+    const test = require('@/test')
+    await test(bcoin.nodeClient, bcoin.walletClient)
+  }
 
   const ucs = new usecase(bcoin.nodeClient, bcoin.walletClient)
 
-  // await ucs.listen()
+  await ucs.listen()
   await ucs.rescanIfEnvPassed()
-  // console.log(await ucs.genAddress())
+  console.log(await ucs.genAddress())
 
   // await ucs.test()
 }
